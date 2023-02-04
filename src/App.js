2f979f93fc8c7fae926b4950e1f541d2d0d82b23
mehdisahin <sahinmehdi23@gmail.com>
2023-02-04 13:37:10 +0300

    add form submission

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import EventDetailPage, { loader as eventDetailLoader } from "./pages/EventDetai
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
